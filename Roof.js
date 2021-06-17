08e9c48f4dba6cd323c7d3a1cdfe1fefd3b58b5e
sanvijain <76484893+sanvijain@users.noreply.github.com>
2021-06-17 23:32:25 +0530

    Update Roof.js

--- Roof.js
+++ Roof.js
@@ -1,19 +1,21 @@
 class Roof{
     constructor(x,y,width,height){
-        var options={
-            isStatic: true
-        }
-        this.body = Bodies.rectangle(x,y,width,height);
+        //var options = {
+          //  isStatic: true,
+            
+        //}
+        this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
         this.width = width;
         this.height = height;
 
         World.add(world,this.body);
     }
     display(){
+        var pos =this.body.position;
         push();
         rectMode(CENTER);
         fill(128,128,128);
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+        rect(pos.x,pos.y,this.width,this.height);
         pop();
     }
 }
