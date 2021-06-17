f9dc1dbd7f13bda72149a89785723bcc37359f8a
sanvijain <76484893+sanvijain@users.noreply.github.com>
2021-06-17 23:31:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,13 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
 
-var bob1; 
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5; 
 var roof;
-var rope1;
-
-//function preload()
-//{
-	
-//}
+var rope1,rope2,rope3,rope4,rope5;
 
 function setup() {
 	createCanvas(800, 700);
@@ -20,13 +16,19 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
-	bob1 = new Bob(300,150,50);
-	roof = new Roof(350,100,300,30);
-	rope1 = new Rope(bob1.body,roof.body,-200,0);
-	
+	roof = new Roof(500,100,500,30);
 
+	bobObject1 = new Bob(200,500,50);
+	bobObject2 = new Bob(300,500,50);
+	bobObject3 = new Bob(400,500,50);
+	bobObject4 = new Bob(500,500,50);
+	bobObject5 = new Bob(600,500,50);
 
+	rope1 = new Rope(bobObject1.body,roof.body,-200,0);
+	rope2 = new Rope(bobObject2.body,roof.body,-100,0);
+	rope3 = new Rope(bobObject3.body,roof.body,0,0);
+	rope4 = new Rope(bobObject4.body,roof.body,100,0);
+	rope5 = new Rope(bobObject5.body,roof.body,200,0);
 
 	Engine.run(engine);
   
@@ -34,15 +36,27 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
+  //rectMode(CENTER);
   background("white");
+  Engine.update(engine);
   
   //drawSprites();
-  bob1.display();
   roof.display();
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
   rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
  
 }
+
 
 
 
